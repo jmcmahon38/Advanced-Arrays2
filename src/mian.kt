dc2e3e206c2c6ee1a4be4ec5aa63c7456eea2edd
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-09-27 17:46:03 -0500

    removed unneeded import

--- src/mian.kt
+++ src/mian.kt
@@ -1,4 +1,3 @@
-import javax.management.openmbean.ArrayType
 val favoriteThings = arrayOf(
     "Raindrops on roses", "Whiskers on kittens", "Bright copper kettles",
     "Warm woolen mittens", "Brown paper packages tied up with strings")
